69beed6130bf3fa947207b2f8092a0f2b3e561d9
ckenanoglu <cihankenanoglu@gmail.com>
2023-01-21 17:46:57 +0300

    code cleanup

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 const choices = ["rock", "paper", "scissors",];
-
-// function getComputerChoice() {
-       // return choices(Math.floor(Math.random() * choices.length));
-// }
-// console.log(playRound);
-
-
 const computerSelection = choices[Math.floor(Math.random()*choices.length)];
        console.log("pc chose " + `${computerSelection}`);     
 
@@ -36,27 +29,3 @@ function playRound(playerSelection, computerSelection) {
        }
 }
 
-
-// const winner = playRound(playerSelection, computerSelection);
-
-       
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
-// console.log(computerSelection);
-// // console.log(computerSelection);
-// function playRound(playerSelection, computerSelection) {
-//        if (yourPoints === 5 || computerPoints === 5 || tie === 5) return; //This stops the game when reaching to 5
-//                if ((computerSelection === "Paper" && playerSelection === "Rock") ||
-//                    (computerSelection === "Scissors" && playerSelection === "Paper") ||
-//                    (computerSelection === "Rock" && playerSelection === "Scissors")) 
-//                     {
-//                } else if ((computerSelection === "Rock" && playerSelection === "Paper") ||
-//                          (computerSelection === "Paper" && playerSelection === "Scissors") ||
-//                          (computerSelection === "Scissors" && playerSelection === "Rock")) 
-//                           {
-//                } else if ((computerSelection === playerSelection)               ) {
-
-//                } else {
-//                    return "not valid";
-//                }}
